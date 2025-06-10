3936962c111db6be7805ecce1a9a1c655d9fa8b2
Ilya <Ptvkzr_YbrekbyfH@mail.ru>
2025-06-10 12:00:38 +0300

    for prod

--- client/src/routes/post/[post_id]/+page.svelte
+++ client/src/routes/post/[post_id]/+page.svelte
@@ -10,7 +10,7 @@
     let role = $state(data.role.trim())
     
     let add_or_delete_like = async () => {
-        let res = await fetch('http://localhost:8080/posts/add_or_delete_like', 
+        let res = await fetch('http://85.198.80.78:8080/posts/add_or_delete_like', 
         {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'}, 
@@ -23,7 +23,7 @@
     }
 
     let delete_post = async () => {
-        let res = await fetch('http://localhost:8080/posts/delete',{
+        let res = await fetch('http://85.198.80.78:8080/posts/delete',{
             method: "DELETE",
             headers: {'Content-Type': 'application/json'}, 
             body: JSON.stringify(post)
@@ -38,7 +38,7 @@
         if(new_comment.length == 0) return
         console.log('add_comment2')
         let body = {content: new_comment, author_id: localStorage.getItem('id'), post_id: post_id}
-        let res = await fetch('http://localhost:8080/comments/create', 
+        let res = await fetch('http://85.198.80.78:8080/comments/create', 
         {
             method: 'POST',
             headers: {'Content-Type': 'application/json'}, 
@@ -49,7 +49,7 @@
         
     }
     let delete_comment = async (comment) => {
-        let res = await fetch('http://localhost:8080/comments/delete', 
+        let res = await fetch('http://85.198.80.78:8080/comments/delete', 
         {
             method: 'DELETE',
             headers: {'Content-Type': 'application/json'}, 
